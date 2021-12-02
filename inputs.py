c1507038aea7729e9713236c66265de4b581d8f4
Max Nadeau <nadeau.max@gmail.com>
2021-12-02 16:29:09 -0500

    a

---
+++ inputs.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# prisoner's dilemma payoff matrix
+pd_p_mat = np.array([[[2, 2], [-3, 0]],
+                     [[0, -3], [1, 1]]])
+
+# chicken
+ch_p_mat = np.array([[[0, 0], [0, 2]],
+                     [[2, 0], [-4, -4]]])
+
+# matching pennies
+mp_p_mat = np.array([[[1, -1], [-1, 1]],
+                     [[-1, 1], [1, -1]]])
+
+# hawk-dove
+hd_p_mat = np.array([[[-1, -1], [10, 0]],
+                     [[0, 10], [5, 5]]])
+
+# hawk-dove
+hd_p_mat2 = np.array([[[-4, -4], [10, 0]],
+                     [[0, 10], [5, 5]]])
+
+# 2-action, uniform initial state population
+unif_pop = []
+for i in range(100):
+    prob_vec = [i/100, 1 - i/100]
+    unif_pop.append(prob_vec)
